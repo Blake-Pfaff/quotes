8b1e43f2b3f33b644354f97cd597ae5ea38e6a1b
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-04-16 10:02:24 -0500

    adds data obj to app file

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function {
+            quotes: [
+                    'Just a quote'
+            ],
+            maxQuotes: 10
+        }
+    }
+</script>
+
+<style>
+</style>
