20c2dc60a5b66fc455b6cd42e5d9d199c67c73ca
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-04-16 10:27:59 -0500

    adds quote to dom

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="container">
-
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
-        data: function {
-            quotes: [
-                    'Just a quote'
-            ],
-            maxQuotes: 10
+        data: function() {
+            return {
+                quotes: [
+                        'Just a quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
         }
     }
 </script>
